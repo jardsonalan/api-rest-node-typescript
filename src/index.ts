483124b32342a823befa6594442824884bf48a3e
Jardson Alan <jardsonalan132@gmail.com>
2025-04-14 20:08:11 -0300

    feat: adicionando configurações de variáveis de ambiente e configurações de build

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { server } from "./server/Server"
 
 // Serve para escutar o servidor
-server.listen(3000, () => console.log('Rodando...'))
+server.listen(process.env.PORT || 3000, () => {
+    console.log(`Rodando na porta: ${process.env.PORT || 3000}`)
+})
