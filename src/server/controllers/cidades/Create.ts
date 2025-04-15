598925b384db845b5a8acf00752bef955e4691d5
Jardson Alan <jardsonalan132@gmail.com>
2025-04-15 19:10:58 -0300

    feat: adicionando validação de dados utilizando o yup

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,15 +1,38 @@
 import { Request, Response } from "express";
+// Biblioteca para fazer validação de dados
+import * as yup from 'yup'
 
 interface ICidade {
-    name: string
+    name: string;
 }
+
+// Schema de validação
+const bodyValidation: yup.ObjectSchema<ICidade> = yup.object().shape({
+    name: yup.string().required().min(3),
+})
 
 // Controller de criação de cidades
 // Tipando o tipo da request - informando que os dados, que o request irá receber, serão os que estão na interface ICidade
-export const create = (req: Request<{}, {}, ICidade>, res: Response) => {
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
     // Dados que estamos recebendo da chamada
-    const data: ICidade = req.body
-    console.log(data.name)
+    const data = req.body
+
+    // Serve para receber os dados
+    let validateData: ICidade | undefined = undefined
+
+    try {
+        validateData = await bodyValidation.validate(data)
+    } catch (error) {
+        const yupError = error as yup.ValidationError
+        
+        return res.json({
+            errors: {
+                default: yupError.message // Retorna uma mensagem de erro
+            }
+        })
+    }
+
+    console.log(validateData)
 
     return res.send('Create')
 }
