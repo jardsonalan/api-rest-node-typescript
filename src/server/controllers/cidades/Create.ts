c4171a274cfab108a5ff7faf6f2096ecd6ae824d
Jardson Alan <jardsonalan132@gmail.com>
2025-04-19 20:00:09 -0300

    test: alteração feita para realizar os testes com a controller Create de cidades

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -28,5 +28,5 @@ export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
 
     console.log(data)
     
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Não implementado')
+    return res.status(StatusCodes.CREATED).json(1)
 }
