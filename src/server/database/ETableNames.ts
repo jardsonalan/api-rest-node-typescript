74d6824eb5fd6f01adc27d9ea80cb74ec7044496
Jardson Alan <jardsonalan132@gmail.com>
2025-04-21 20:58:13 -0300

    feat: criação da migration para criar/deletar a tabela cidade

---
+++ src/server/database/ETableNames.ts
@@ -0,0 +1,4 @@
+// Serve para armazenar o nome de todas as tabelas do banco de dados
+export enum ETableNames {
+    cidade = 'cidade'
+}
---
+++ src/server/database/migrations/0000_create_cidade.ts
@@ -0,0 +1,26 @@
+import type { Knex } from "knex";
+import { ETableNames } from "../ETableNames";
+
+// Serve para criar a tabela cidades
+export async function up(knex: Knex) {
+    return knex
+    .schema
+    .createTable(ETableNames.cidade, table => {
+        table.bigIncrements('id').primary().index()
+        table.string('nome', 150).index().notNullable()
+
+        table.comment('Tabela usada para armazenar cidades do sistema.')
+    })
+    .then(() => {
+        console.log(`# Created table ${ETableNames.cidade}`)
+    })
+}
+
+// Serve para deletar a tabela cidades
+export async function down(knex: Knex) {
+    return knex.schema
+    .dropTable(ETableNames.cidade)
+    .then(() => {
+        console.log(`# Dropped table ${ETableNames.cidade}`)
+    })
+}
