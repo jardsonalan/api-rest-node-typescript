6b85c592f82f3d2de47b74fb4c2e723a372ef775
Jardson Alan <jardsonalan132@gmail.com>
2025-04-28 20:04:12 -0300

    refactor: corrigindo o arquivo de teste do jest

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -2,6 +2,17 @@
 import supertest from "supertest";
 // Importação do server
 import { server } from "../src/server/Server";
+import { Knex } from "../src/server/database/knex";
+
+// Serve para esperar o Knex criar as migrações das tabelas, antes de rodar os testes
+beforeAll(async () => {
+  await Knex.migrate.latest()
+})
+
+// Serve para esperar a desconexão com o Knex, após os testes, e não ficar uma conexão ligada após encerrar os testes
+afterAll(async () => {
+  await Knex.destroy()
+})
 
 // Exportação da variável que irá servir para realizar os testes
 export const testServer = supertest(server)
