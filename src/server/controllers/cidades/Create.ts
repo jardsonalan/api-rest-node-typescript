a8162563900cf519d1a2bf70b647491372fb79dd
Jardson Alan <jardsonalan132@gmail.com>
2025-04-28 19:46:48 -0300

    feat: criação da provider para cidades

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -4,6 +4,7 @@ import * as yup from 'yup'
 import { validation } from "../../shared/middlewares";
 import { StatusCodes } from "http-status-codes";
 import { ICidade } from "../../database/models";
+import { CidadesProvider } from "../../database/providers/cidades";
 
 // Omit: serve para omitir/esconder algum campo
 interface IBodyProps extends Omit<ICidade, 'id'> {}
@@ -15,7 +16,7 @@ interface IBodyProps extends Omit<ICidade, 'id'> {}
 export const createValidation = validation((getSchema) => ({
     // Schema de validação
     body: getSchema<IBodyProps>(yup.object().shape({
-        nome: yup.string().required().min(3),
+        nome: yup.string().required().min(3).max(150),
     })),
 }))
 
@@ -23,10 +24,21 @@ export const createValidation = validation((getSchema) => ({
 // Esse método só será executado, se os dados passarem na validação do middleware
 // Tipando o tipo da request - informando que os dados, que o request irá receber, serão os que estão na interface ICidade
 export const create = async (req: Request<{}, {}, IBodyProps>, res: Response) => {
-    // Corpo da chamada
-    const data = req.body
+    // Tentativa de criar uma cidade
+    // Retorna se a criação da cidade foi bem sucedida, ou não, dentro do result
+    const result = await CidadesProvider.create(req.body)
 
-    console.log(data)
+    // Verifica se result é uma instância de um Error
+    // Ou seja, se tiver acontecido algum error
+    if (result instanceof Error) {
+      // Retorna um error interno de servidor, com uma mensagem de error padrão
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        errors: {
+          dafault: result.message
+        }
+      })
+    }
     
-    return res.status(StatusCodes.CREATED).json(1)
+    // Caso a criação foi bem sucedida, retorna o result em um json
+    return res.status(StatusCodes.CREATED).json(result)
 }
