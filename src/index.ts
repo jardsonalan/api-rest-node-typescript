8fd4ce34029979935a19883a6eaf290b7f96fa90
Jardson Alan <jardsonalan132@gmail.com>
2025-05-13 17:57:53 -0300

    fix: resolvendo problemas de produção

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
+import { Knex } from "./server/database/knex"
 import { server } from "./server/Server"
 
 // Serve para escutar o servidor
-server.listen(process.env.PORT || 3000, () => {
-    console.log(`Rodando na porta: ${process.env.PORT || 3000}`)
-})
+const startServer = () => {
+  server.listen(process.env.PORT || 3000, () => {
+      console.log(`Rodando na porta: ${process.env.PORT || 3000}`)
+  })
+}
+
+// Verifica se o cenário que a aplicação está rodando, é no cenário de localhost
+// Se for em produção, roda as migrations ao iniciar a aplicação
+if (process.env.IS_LOCALHOST !== 'true') {
+  // Serve para rodar as migrations ao iniciar a aplicação
+  // Roda primeiro as migrations e depois inicializa o servidor
+  Knex.migrate.latest()
+    .then(() => {
+      startServer()
+    })
+    .catch(console.log)
+} else {
+  startServer()
+}
