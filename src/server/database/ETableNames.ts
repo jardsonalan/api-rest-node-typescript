53fd7ef7d5ce3d105ea865e227e6ce06c8c51f5b
Jardson Alan <jardsonalan132@gmail.com>
2025-05-15 19:37:22 -0300

    feat: criação do model e migration de usuários

--- src/server/database/ETableNames.ts
+++ src/server/database/ETableNames.ts
@@ -1,5 +1,6 @@
 // Serve para armazenar o nome de todas as tabelas do banco de dados
 export enum ETableNames {
     cidade = 'cidade',
-    pessoa = 'pessoa'
+    pessoa = 'pessoa',
+    usuario = 'usuario'
 }
---
+++ src/server/database/migrations/0002_create_usuario.ts
@@ -0,0 +1,28 @@
+import type { Knex } from "knex";
+import { ETableNames } from "../ETableNames";
+
+// Serve para criar a tabela usuarios
+export async function up(knex: Knex) {
+  return knex
+  .schema
+  .createTable(ETableNames.usuario, table => {
+    table.bigIncrements('id').primary().index()
+    table.string('nome').notNullable().checkLength('>', 3)
+    table.string('email').index().unique().notNullable().checkLength('>', 5)
+    table.string('senha').notNullable().checkLength('>', 6)
+
+    table.comment('Tabela usada para armazenar usuários do sistema.')
+  })
+  .then(() => {
+      console.log(`# Created table ${ETableNames.usuario}`)
+  })
+}
+
+// Serve para deletar a tabela usuarios
+export async function down(knex: Knex) {
+  return knex.schema
+  .dropTable(ETableNames.usuario)
+  .then(() => {
+    console.log(`# Dropped table ${ETableNames.usuario}`)
+  })
+}
