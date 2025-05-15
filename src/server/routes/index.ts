47d1b660ee8983cee36c51b1126de147dcf192e0
Jardson Alan <jardsonalan132@gmail.com>
2025-05-14 22:04:19 -0300

    feat: criação das rotas de pessoas

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -2,8 +2,7 @@
 // Podemos especificar todas as nossas rotas
 import { Router } from "express";
 // Biblioteca que fornece os status code com mais detalhes
-import { StatusCodes } from 'http-status-codes'
-import { CidadesController } from "../controllers";
+import { CidadesController, PessoasController } from "../controllers";
 
 const router = Router()
 
@@ -13,11 +12,19 @@ router.get('/', (request, response) => {
     return response.send('Funcionando')
 })
 
+// Rotas de cidades
 router.get('/cidades', CidadesController.getAllValidation, CidadesController.getAll)
 // Executa a validação dos dados, antes da criação dos dados
 router.post('/cidades', CidadesController.createValidation, CidadesController.create)
 router.get('/cidades/:id', CidadesController.getByIdValidation, CidadesController.getById)
 router.put('/cidades/:id', CidadesController.updateByIdValidation, CidadesController.updateById)
 router.delete('/cidades/:id', CidadesController.deleteByIdValidation, CidadesController.deleteById)
+
+// Rotas de pessoas
+router.get('/pessoas', PessoasController.getAllValidation, PessoasController.getAll)
+router.post('/pessoas', PessoasController.createValidation, PessoasController.create)
+router.get('/pessoas/:id', PessoasController.getByIdValidation, PessoasController.getById)
+router.put('/pessoas/:id', PessoasController.updateByIdValidation, PessoasController.updateById)
+router.delete('/pessoas/:id', PessoasController.deleteByIdValidation, PessoasController.deleteById)
 
 export { router }
