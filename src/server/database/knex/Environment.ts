c2656afb48f43d7657d96e7eae11ffb33bf7aca7
Jardson Alan <jardsonalan132@gmail.com>
2025-05-19 15:19:53 -0300

    feat: adicionando as configurações de ambiente para produção

--- src/server/database/knex/Environment.ts
+++ src/server/database/knex/Environment.ts
@@ -2,31 +2,45 @@ import { Knex } from "knex"
 import path from 'path'
 
 export const development: Knex.Config = {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-        filename: path.resolve(__dirname, '..', '..', '..', '..', 'database.sqlite')
-    },
-    migrations: {
-        directory: path.resolve(__dirname, '..', 'migrations')
-    },
-    seeds: {
-        directory: path.resolve(__dirname, '..', 'seeds')
-    },
-    pool: {
-        afterCreate: (connection: any, done: Function) => {
-            // Ativa as chaves estrageiras dentro do sqlite
-            connection.run('PRAGMA foreign_keys = ON')
-            done()
-        }
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: path.resolve(__dirname, '..', '..', '..', '..', 'database.sqlite')
+  },
+  migrations: {
+    directory: path.resolve(__dirname, '..', 'migrations')
+  },
+  seeds: {
+    directory: path.resolve(__dirname, '..', 'seeds')
+  },
+  pool: {
+    afterCreate: (connection: any, done: Function) => {
+      // Ativa as chaves estrageiras dentro do sqlite
+      connection.run('PRAGMA foreign_keys = ON')
+      done()
     }
+  }
 }
 
 export const test: Knex.Config = {
-    ...development,
-    connection: ':memory:'
+  ...development,
+  connection: ':memory:'
 }
 
 export const production: Knex.Config = {
-    ...development
+  client: 'pg',
+  migrations: {
+    directory: path.resolve(__dirname, '..', 'migrations')
+  },
+  seeds: {
+    directory: path.resolve(__dirname, '..', 'seeds')
+  },
+  connection: {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: Number(process.env.DATABASE_PORT || 5432),
+    ssl: { rejectUnauthorized: false },
+  },
 }
--- src/server/database/knex/index.ts
+++ src/server/database/knex/index.ts
@@ -1,5 +1,13 @@
 import knex from "knex";
+import pg from 'pg'
+import 'dotenv/config'
 import { development, production, test } from "./Environment";
+
+// Verifica se a aplicação está rodando em produção
+if (process.env.NODE_ENV === 'production') {
+  // Faz o tratamento dos dados como inteiro
+  pg.types.setTypeParser(20, 'text', parseInt)
+}
 
 const getEnvironment = () => {
     switch (process.env.NODE_ENV) {
