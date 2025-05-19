ba5bafb9a4e9ff19331f87b5a47022287087f192
Jardson Alan <jardsonalan132@gmail.com>
2025-05-19 17:16:27 -0300

    feat: adicionando a configuração do cors

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,10 +1,17 @@
 import express from 'express';
+import cors from 'cors'
 import 'dotenv/config'
 import './shared/services/TranslationsYup'
 import { router } from './routes';
 
 // Instância do servidor
 const server = express()
+
+// Configuração do cors
+server.use(cors({
+  // origin: endereços que podem fazer chamadas para o backend
+  origin: process.env.ENABLED_CORS?.split(';') || []
+}))
 
 // Serve para apresentar os dados, passados no Body, no terminal
 server.use(express.json())
